a9a1ca8e08d4f08e4786f8a55e6bd69485bb1555
hkaur02 <hkaur@udel.edu>
2023-10-22 10:35:32 -0400

    notepad added + splitview

--- technica2024/src/App.jsx
+++ technica2024/src/App.jsx
@@ -1,20 +1,20 @@
 import "./App.css";
+import "./index.css";
 import AudioRecorder from "./AudioRecorder";
+import SplitPane from 'react-split-pane';
+ 
 
 const App = () => {
-    /* let [recordOption, setRecordOption] = useState("video"); 
-    const toggleRecordOption = (type) => {
-        return () => {
-            setRecordOption(type);
-        }; 
-    }; */
     return (
+      <SplitPane split="vertical" minSize={50} defaultSize={1000}>
         <div>
-            <h1>Note Helper</h1>
+            <h1>NeuroNotes</h1>
             <div>
                 {<AudioRecorder />}
             </div>
+            <textarea id="user_message" rows="30" cols="70"></textarea>
         </div>
+        </SplitPane>
     );
 };
 

8948e674295d79b418bb5a5f1356757c9f1ad556
Brielle Hina <bhina@udel.edu>
2023-10-22 10:45:36 -0400

    added other side of split

--- technica2024/src/App.jsx
+++ technica2024/src/App.jsx
@@ -6,15 +6,17 @@ import SplitPane from 'react-split-pane';
 
 const App = () => {
     return (
-      <SplitPane split="vertical" minSize={50} defaultSize={1000}>
-        <div>
-            <h1>NeuroNotes</h1>
+      <div><SplitPane split="vertical" minSize={50} defaultSize={1000}>
             <div>
-                {<AudioRecorder />}
+                <h1>NeuroNotes</h1>
+                <div>
+                    {<AudioRecorder />}
+                </div>
+                <textarea id="user_message" rows="30" cols="70"></textarea>
             </div>
-            <textarea id="user_message" rows="30" cols="70"></textarea>
-        </div>
-        </SplitPane>
+        </SplitPane><SplitPane split="vertical" minSize={50} defaultSize={1000}>
+                <div><h2>Block Game</h2></div>
+            </SplitPane></div>
     );
 };
 
